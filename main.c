feea58f13c535a9c5163f15a8daf0f44c8affc11
PJL <pauljohannlorenz@proton.me>
2026-08-11 11:04:05 +0200

    Refactor to use controller

--- main.c
+++ main.c
@@ -1,11 +1,22 @@
 #include <stdio.h>
-#include "mseLoss.h"
+#include "controller.h"
 
 int main() {
-    
-    float loss = mseLoss(0, 0);
+    float features[] = {3.5, 3.69, 3.44, 3.43, 4.34, 4.42, 2.37};
+    float label[] = {18, 15, 18, 16, 15, 14, 24};
+    int numberOfValues;
+    float startingWeight;
+    float startingBias;
 
-    printf("MSE Loss: %f", loss);
+    numberOfValues = sizeof(features) / sizeof(features[0]);
+    startingWeight = 0;
+    startingBias = 0;
+
+    printf("Number of Values: %d\n", numberOfValues);
+    printf("Starting Weight: %.3f\n", startingWeight);
+    printf("Starting Bias: %.3f\n", startingBias);
+
+    controller(features, label, startingWeight, startingBias, numberOfValues);
     
     return 0;
 }
