ff6f0a7658700bc96fbd1756155d48a88fcba272
PJL <pauljohannlorenz@proton.me>
2026-08-11 11:40:11 +0200

    Initial commit

---
+++ biasDerivative.c
@@ -0,0 +1,47 @@
+// First we sum each predicted value minus the actual value 
+// and then multiply it by two. 
+// Then we divide the sum by the number of examples.
+// Taken from: https://developers.google.com/machine-learning/crash-course/linear-regression/gradient-descent
+#include <stdio.h>
+#include "biasDerivative.h"
+
+float biasDerivative(float predictedValue[], float actualValue[], int numberOfValues) {
+    float sum;                          // Sum of all errors * 2
+    float error[numberOfValues];        // Array containing all calculated errors
+    int i;                              // Index
+    float result;                       // Result being returned to caller
+    float errorFeature[numberOfValues]; // Array containing all error * 2
+    float weightDerivative;             // Actual calculated weightDerivative
+    
+    sum = 0;
+
+    // Calculates the error via predicted value - actual value
+    for (i = 0; i < numberOfValues; i++) {
+        error[i] = predictedValue[i] - actualValue[i];
+        printf("Error: %.1f \n", error[i]);
+    }
+    i = 0;
+    printf("\n");
+    printf("\n");
+
+    // Calculates Error * 2
+    for (i = 0; i < numberOfValues; i++) {
+        errorFeature[i] = error[i] * 2;
+        printf("Error * 2: %.1f\n", errorFeature[i]);
+    }
+    i = 0;
+    printf("\n");
+
+    // Calculates the sum of indexes of errorFeature[]
+    for (i = 0; i < numberOfValues; i++) {
+        sum = sum + errorFeature[i];
+    }
+    i = 0;
+
+    // Calculates the actual weight derivative
+    weightDerivative = sum / numberOfValues;
+
+    result = weightDerivative;
+
+    return result;
+}
---
+++ biasDerivative.h
@@ -0,0 +1,10 @@
+// Taken from Brave Search AI summary
+// Cited source: https://stackoverflow.com/questions/13435134/include-other-c-programs
+
+#ifndef BIASDERIVATIVE_H
+#define BIASDERIVATIVE_H
+
+/* Function to calculate the bias derivative */
+float biasDerivative(float predictedValue[], float actualValue[], int numberOfValues);
+
+#endif   
