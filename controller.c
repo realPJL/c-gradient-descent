8c9c86f037ceb01f3142bc96cd4b08f4a747d294
PJL <pauljohannlorenz@proton.me>
2026-08-13 09:42:46 +0200

    Add loop

--- controller.c
+++ controller.c
@@ -3,7 +3,7 @@
 #include "weightDerivative.h"
 #include "biasDerivative.h"
 
-int controller(float features[], float label[], float weight, float bias, int numberOfValues, float learningRate) {
+int controller(float features[], float label[], float weight, float bias, int numberOfValues, float learningRate, int maxIterations) {
     float loss;
     float weightGradient;
     float biasGradient;
@@ -13,35 +13,42 @@ int controller(float features[], float label[], float weight, float bias, int nu
     float newBias;
     int i;
     float predictedValues[numberOfValues];
+    int iteration;
 
-    // Calculates the predicted values before sending them to mseLoss(), weightDerivative() and biasDerivative()
-    // Uses y' = b + w * x
-    for (i = 0; i < numberOfValues; i++) {
-        predictedValues[i] = bias + (weight * features[i]);
-        printf("%.3f | ", predictedValues[i]);
+    for (iteration = 0; iteration < maxIterations; iteration++) {
+        printf("\n\n---ITERATION: %d---\n\n", iteration);
+
+        // Calculates the predicted values before sending them to mseLoss(), weightDerivative() and biasDerivative()
+        // Uses y' = b + w * x
+        for (i = 0; i < numberOfValues; i++) {
+            predictedValues[i] = bias + (weight * features[i]);
+            printf("%.3f | ", predictedValues[i]);
+        }
+        printf("\n");
+
+        // Calculates MSE Loss
+        loss = mseLoss(features, label, predictedValues, weight, bias, numberOfValues);
+        weightGradient = weightDerivative(predictedValues, label, features, numberOfValues);
+        biasGradient = biasDerivative(predictedValues, label, numberOfValues);
+
+        weightChange = weightGradient * learningRate;
+        biasChange = biasGradient * learningRate;
+
+        newWeight = weight - weightChange;
+        newBias = bias - biasChange;
+
+        printf("MSE Loss: %f\n", loss);
+        printf("Weight Derivative: %f\n", weightGradient);
+        printf("Bias Derivative: %f\n", biasGradient);
+        printf("\n");
+        printf("\n");
+        printf("Bias change: %.3f\n", biasChange);
+        printf("Weight change: %.3f\n", weightChange);
+        printf("New Weight: %.3f\n", newWeight);
+        printf("New Bias: %.3f\n", newBias);
+
+        // if last mse losses are around current mse loss -> break;
     }
-    printf("\n");
-
-    // Calculates MSE Loss
-    loss = mseLoss(features, label, predictedValues, weight, bias, numberOfValues);
-    weightGradient = weightDerivative(predictedValues, label, features, numberOfValues);
-    biasGradient = biasDerivative(predictedValues, label, numberOfValues);
-
-    weightChange = weightGradient * learningRate;
-    biasChange = biasGradient * learningRate;
-
-    newWeight = weight - weightChange;
-    newBias = bias - biasChange;
-
-    printf("MSE Loss: %f\n", loss);
-    printf("Weight Derivative: %f\n", weightGradient);
-    printf("Bias Derivative: %f\n", biasGradient);
-    printf("\n");
-    printf("\n");
-    printf("Bias change: %.3f\n", biasChange);
-    printf("Weight change: %.3f\n", weightChange);
-    printf("New Weight: %.3f\n", newWeight);
-    printf("New Bias: %.3f\n", newBias);
 
     return 0;
 }
