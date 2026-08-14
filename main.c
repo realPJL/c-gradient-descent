4fb0dccf0a2c95b324b78a16b999c16e7ff70252
PJL <pauljohannlorenz@proton.me>
2026-08-14 09:44:41 +0200

    Increase iterations

--- main.c
+++ main.c
@@ -14,7 +14,7 @@ int main() {
     startingWeight = 0;
     startingBias = 0;
     learningRate = 0.01;
-    maxIterations = 20;
+    maxIterations = 8000;
 
     printf("Number of Values: %d\n", numberOfValues);
     printf("Starting Weight: %.3f\n", startingWeight);
