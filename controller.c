a04ca9b1b045c7a1e14ae8e955db6f134fdc13df
PJL <pauljohannlorenz@proton.me>
2026-08-14 09:45:07 +0200

    Refactor to only use one function and pointers

--- controller.c
+++ controller.c
@@ -3,30 +3,21 @@
 #include "weightDerivative.h"
 #include "biasDerivative.h"
 
-void firstCalculation(float features[], float label[], float startingWeight, float startingBias, int numberOfValues, float learningRate);
-void nextCalculation(float features[], float label[], float newWeight, float newBias, int numberOfValues, float learningRate);
-
-float loss;
-float weightGradient;
-float biasGradient;
-float weightChange;
-float biasChange;
-float newWeight;
-float newBias;
-float predictedValues[7];
-
+void calculateGradientDescent(float features[], float label[], float *weight, float *bias, int numberOfValues, float learningRate);
 
 int controller(float features[], float label[], float startingWeight, float startingBias, int numberOfValues, float learningRate, int maxIterations) {
     int iteration;
+    float weight;
+    float bias;
+
+    weight = startingWeight;
+    bias = startingBias;
+
 
     for (iteration = 0; iteration < maxIterations; iteration++) {
         printf("\n\n---ITERATION: %d---\n\n", iteration);
         
-        if (iteration == 0) {
-            firstCalculation(features, label, startingWeight, startingBias, numberOfValues, learningRate);
-        } else {
-            nextCalculation(features, label, newWeight, newBias, numberOfValues, learningRate);
-        }
+        calculateGradientDescent(features, label, &weight, &bias, numberOfValues, learningRate);
 
         // if last mse losses are around current mse loss -> break;
     }
@@ -34,62 +25,34 @@ int controller(float features[], float label[], float startingWeight, float star
     return 0;
 }
 
-// Does the first round of calculations using the starting weight/bias. Only executed once each session.
-void firstCalculation(float features[], float label[], float startingWeight, float startingBias, int numberOfValues, float learningRate) {
+// Calculates weight and bias resulting in the smallest loss. Executes multiple times.
+void calculateGradientDescent(float features[], float label[], float *weight, float *bias, int numberOfValues, float learningRate) {
     int i;
-
-    // Calculates the predicted values before sending them to mseLoss(), weightDerivative() and biasDerivative()
-    // Uses y' = b + w * x
-    for (i = 0; i < numberOfValues; i++) {
-        predictedValues[i] = startingBias + (startingWeight * features[i]);
-        printf("%.3f | ", predictedValues[i]);
-    }
-    printf("\n");
-
-    // Calculates MSE Loss
-    loss = mseLoss(features, label, predictedValues, startingWeight, startingBias, numberOfValues);
-    weightGradient = weightDerivative(predictedValues, label, features, numberOfValues);
-    biasGradient = biasDerivative(predictedValues, label, numberOfValues);
-
-    weightChange = weightGradient * learningRate;
-    biasChange = biasGradient * learningRate;
-
-    newWeight = startingWeight - weightChange;
-    newBias = startingBias - biasChange;
-
-    printf("MSE Loss: %f\n", loss);
-    printf("Weight Derivative: %f\n", weightGradient);
-    printf("Bias Derivative: %f\n", biasGradient);
-    printf("\n");
-    printf("\n");
-    printf("Bias change: %.3f\n", biasChange);
-    printf("Weight change: %.3f\n", weightChange);
-    printf("New Weight: %.3f\n", newWeight);
-    printf("New Bias: %.3f\n", newBias);
-}
-
-// Does rounds 2 - maxIterations of calculations using the calculated/updated weight/bias. Executes multiple times.
-void nextCalculation(float features[], float label[], float newWeight, float newBias, int numberOfValues, float learningRate) {
-    int i;
+    float predictedValues[numberOfValues];
+    float loss;
+    float weightGradient;
+    float biasGradient;
+    float weightChange;
+    float biasChange;
     
     // Calculates the predicted values before sending them to mseLoss(), weightDerivative() and biasDerivative()
     // Uses y' = b + w * x
     for (i = 0; i < numberOfValues; i++) {
-        predictedValues[i] = newBias + (newWeight * features[i]);
-        printf("%.3f | ", predictedValues[i]);
+        predictedValues[i] = *bias + (*weight * features[i]);
+        /* printf("%.3f | ", predictedValues[i]); */
     }
     printf("\n");
 
     // Calculates MSE Loss
-    loss = mseLoss(features, label, predictedValues, newWeight, newBias, numberOfValues);
+    loss = mseLoss(features, label, predictedValues, *weight, *bias, numberOfValues);
     weightGradient = weightDerivative(predictedValues, label, features, numberOfValues);
     biasGradient = biasDerivative(predictedValues, label, numberOfValues);
 
     weightChange = weightGradient * learningRate;
     biasChange = biasGradient * learningRate;
 
-    newWeight = newWeight - weightChange;
-    newBias = newBias - biasChange;
+    *weight = *weight - weightChange;
+    *bias = *bias - biasChange;
 
     printf("MSE Loss: %f\n", loss);
     printf("Weight Derivative: %f\n", weightGradient);
@@ -98,6 +61,6 @@ void nextCalculation(float features[], float label[], float newWeight, float new
     printf("\n");
     printf("Bias change: %.3f\n", biasChange);
     printf("Weight change: %.3f\n", weightChange);
-    printf("New Weight: %.3f\n", newWeight);
-    printf("New Bias: %.3f\n", newBias);
+    printf("New Weight: %.3f\n", *weight);
+    printf("New Bias: %.3f\n", *bias);
 }
